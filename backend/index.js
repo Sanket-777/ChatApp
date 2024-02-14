7598dc3a0c40a6314e26d32c66a9e1b3ea6e6525
Sanket-777 <Ksanket114402@gmail.com>
2024-02-14 16:59:49 +0530

    cors bug

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,21 @@ import { Server } from "socket.io";
 
 const httpServer = createServer();
 
-const io = new Server(httpServer.listeners, {
+const io = new Server(httpServer, {
   cors: {
-    origin: '*',
+    origin: "https://randomchat-zeta.vercel.app/",
+    methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    credentials: true
   },
 });
 
+io.engine.on("headers", (headers, req) => {
+  headers["Access-Control-Allow-Origin"] = "https://randomchat-zeta.vercel.app/";
+  headers["Access-Control-Allow-Headers"] =
+    "origin, x-requested-with, content-type";
+  headers["Access-Control-Allow-Methodsn"] = "PUT, GET, POST, DELETE, OPTIONS";
+});
 io.on("connection", (socket) => {
   console.log(`User ${socket.id} just connected `);
 
