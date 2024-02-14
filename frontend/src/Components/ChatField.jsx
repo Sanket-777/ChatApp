3b639ccea73b6d2f7525615c96295db453d42ce6
Sanket-777 <Ksanket114402@gmail.com>
2024-02-14 15:06:03 +0530

    added chatfield component

---
+++ frontend/src/Components/ChatField.jsx
@@ -0,0 +1,26 @@
+export default function ChatField({ inputValue, setInputValue, sendMessage }) {
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    return (
+        <div className="flex p-2 font-medium rounded-b-lg bg-yellow-200">
+            <input
+                className="w-full h-full p-2 rounded-md"
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+            <button
+                className="ml-4 focus:outline-none transform transition duration-300 hover:scale-110 active:scale-90"
+                onClick={sendMessage} 
+            >
+                <div className="h-8 w-8 flex items-center justify-center rounded-full bg-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                    </svg>
+                </div>
+            </button>
+        </div>
+    );
+}
---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <>
+            <div className="flex items-center justify-center p-3 font-medium rounded-t-lg bg-yellow-200">
+                <label className="text-2xl">Chat Application</label>
+            </div>
+
+        </>
+    )
+}
