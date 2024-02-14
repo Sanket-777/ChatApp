2fd5d969d9305217bba7c4f52f10a61ce28a6208
Sanket-777 <Ksanket114402@gmail.com>
2024-02-14 16:55:50 +0530

    cors bug

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ import { Server } from "socket.io";
 
 const httpServer = createServer();
 
-const io = new Server(httpServer, {
+const io = new Server(httpServer.listeners, {
   cors: {
-    origin: "*",
+    origin: '*',
   },
 });
 
