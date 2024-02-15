aee7167c2328599c99fb38b7ca91972386c11197
Sanket-777 <Ksanket114402@gmail.com>
2024-02-15 14:14:29 +0530

    added welcome page and routes

--- frontend/src/Pages/ChatScreen.jsx
+++ frontend/src/Pages/ChatScreen.jsx
@@ -1,38 +1,43 @@
+/* eslint-disable no-unused-vars */
 import ChatBox from "../Components/ChatBox";
 import ChatField from "../Components/ChatField";
 import Header from "../Components/Header";
 import { useState, useEffect } from 'react';
-import io from 'socket.io-client';
 const socket = io('https://chatapp-backend-mxxy.onrender.com');
 // const socket = io('https://chat-app-weld-two.vercel.app');
-// const socket = io('http://localhost:4000'); // Connect to server
+// const socket = io('http://localhost:4000'); // Connect to server locally
 import 'react-toastify/dist/ReactToastify.css';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { io } from "socket.io-client";
+import Welcome from "./Welcome";
 
 export default function ChatScreen() {
     const [messages, setMessages] = useState([]);
     const [inputValue, setInputValue] = useState('');
     const [people, setPeople] = useState(0);
+    const [name, setName] = useState("");
+    const [Start, setStartuseState] = useState("");
+
 
     useEffect(() => {
-        // Listen for incoming messages from server
+
         socket.on('message', (data) => {
-            console.log(data + "msg data")
             setMessages((prevMessages) => [...prevMessages, data]);
         });
 
+
+        console.log(socket.id)
         socket.on('newuser', (data) => {
             console.log(data + "msg data")
-            // people++;
             toast.info(data);
+
 
         });
 
         socket.on('userleft', (data) => {
             console.log(data + "msg data")
-            // people--;
             toast.info(data);
 
         });
@@ -47,8 +52,12 @@ export default function ChatScreen() {
             setPeople(data);
         });
 
-        return () => socket.disconnect();
+        socket.on('chatHistory', (history) => {
+            setMessages(history);
 
+        });
+
+        return (() => socket.disconnect())
     }, []);
 
     const sendMessage = () => {
@@ -57,13 +66,13 @@ export default function ChatScreen() {
     };
     return (
         <>
-
             <div className="bg-white  rounded-lg h-5/6  w-3/6 flex flex-col justify-between shadow-xl border-solid border-1" >
-                <Header people={people} />
+                <Header label={"Chat Application"} people={people} />
                 <ChatBox messages={messages} />
                 <ChatField inputValue={inputValue}
                     setInputValue={setInputValue}
                     sendMessage={sendMessage} />
+
             </div>
             <ToastContainer
                 autoClose={3000}
@@ -75,7 +84,6 @@ export default function ChatScreen() {
                 draggable
                 pauseOnHover
             />
-
         </>
 
     )
---
+++ frontend/src/Pages/Welcome.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import Header from "../Components/Header"
+import { useNavigate } from "react-router-dom";
+
+export default function Welcome() {
+    const navigateTo = useNavigate();
+    const handleStartChatting = () => {
+        navigateTo("/chat"); // Call navigateTo when the button is clicked
+    };
+    return (
+        <div className="bg-white  rounded-lg h-2/6  w-1/8 flex flex-col   shadow-xl border-solid border-1" >
+            <Header label={"Wel-Come to Random Chat Application"} />
+            <button onClick={handleStartChatting} className="bg-yellow-300 p-4 rounded-md font-bold text-lg m-auto focus:outline-none transform transition duration-300 hover:scale-110 active:scale-90">
+                Start Chatting
+            </button>
+        </div>
+    )
+}
