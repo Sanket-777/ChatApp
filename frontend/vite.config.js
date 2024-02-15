1ad4fb7908d42791026b66f13b80dea26fe393e3
Sanket-777 <Ksanket114402@gmail.com>
2024-02-15 14:59:30 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
+  external: ["react", "react-dom", "react-emoji-picker", "emoji-mart"],
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+    },
+  },
 })
